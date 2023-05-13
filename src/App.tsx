74f3ec06904cecaaca74d0e365ff6444405f7698
BasilAntonov <antonov.basil@gmail.com>
2023-05-13 10:26:39 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Stack from "@mui/material/Stack";
+import Header from "./Header";
+import ListPaper from "./ListPaper";
+
+const App = () => {
+  return (
+    <Stack>
+      <Header />
+      <ListPaper />
+    </Stack>
+  );
+};
+
+export default App;
